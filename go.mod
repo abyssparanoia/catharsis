61fc496a203a7a8cede16ff77e21c11072aa5595
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2019-11-20 23:09:14 +0900

    feat: added zap logger config

--- go.mod
+++ go.mod
@@ -5,8 +5,7 @@ go 1.12
 require (
 	github.com/golang/protobuf v1.3.2
 	github.com/rs/zerolog v1.17.2
-	golang.org/x/lint v0.0.0-20190313153728-d0100b6bd8b3 // indirect
+	go.uber.org/zap v1.13.0
 	google.golang.org/appengine v1.6.5
 	google.golang.org/grpc v1.25.1
-	honnef.co/go/tools v0.0.0-20190523083050-ea95bdfd59fc // indirect
 )
---
+++ pkg/log/config.go
@@ -0,0 +1,31 @@
+package log
+
+import "go.uber.org/zap"
+
+// NewDevelopmentConfig ... development config
+func NewDevelopmentConfig() zap.Config {
+	return zap.Config{
+		Level:            zap.NewAtomicLevelAt(zap.DebugLevel),
+		Development:      true,
+		Encoding:         "console",
+		EncoderConfig:    zap.NewDevelopmentEncoderConfig(),
+		OutputPaths:      []string{"stderr"},
+		ErrorOutputPaths: []string{"stderr"},
+	}
+}
+
+// NewProductionConfig ... production config
+func NewProductionConfig() zap.Config {
+	return zap.Config{
+		Level:       zap.NewAtomicLevelAt(zap.InfoLevel),
+		Development: false,
+		Sampling: &zap.SamplingConfig{
+			Initial:    100,
+			Thereafter: 100,
+		},
+		Encoding:         "json",
+		EncoderConfig:    zap.NewProductionEncoderConfig(),
+		OutputPaths:      []string{"stderr"},
+		ErrorOutputPaths: []string{"stderr"},
+	}
+}

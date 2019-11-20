8c6ef9edd8e10175e17955f4cb8f263c715dc0ec
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2019-11-20 21:27:24 +0900

    feat: impl authentication handler

---
+++ authentication/handler/authentication.go
@@ -0,0 +1,34 @@
+package handler
+
+import (
+	"context"
+	"log"
+
+	pb "github.com/abyssparanoia/catharsis/proto/authentication"
+
+	"github.com/abyssparanoia/catharsis/authentication/service"
+)
+
+// AuthenticationHandler ... authentication handler struct
+type AuthenticationHandler struct {
+	authenticationServive service.Authentication
+}
+
+// SignIn ... sign in handler
+func (h *AuthenticationHandler) SignIn(ctx context.Context, req *pb.SignInMessageRequest) (*pb.SignInMessageResponse, error) {
+
+	// TODO: addtinal validate or call other outer service
+
+	accessToken, refreshToken, err := h.authenticationServive.SignIn(ctx, req.UserId, req.Password)
+	if err != nil {
+		log.Fatalf("h.authenticationServive.SignIn: %s", err)
+		return nil, err
+	}
+
+	return &pb.SignInMessageResponse{AccessToken: accessToken, RefreshToken: refreshToken}, nil
+}
+
+// NewAuthenticationHandler ... get authentication handler
+func NewAuthenticationHandler(authenticationServive service.Authentication) *AuthenticationHandler {
+	return &AuthenticationHandler{authenticationServive}
+}

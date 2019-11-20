15f88ea4c4f9a57ccb2b8ba03aadb523ef8552f9
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2019-11-20 19:31:06 +0900

    feat: impl entity func

--- authentication/infrastructure/internal/entity/user.go
+++ authentication/infrastructure/internal/entity/user.go
@@ -1,8 +1,24 @@
 package entity
 
+import "github.com/abyssparanoia/catharsis/authentication/domain/model"
+
 // User ... user entity
 type User struct {
 	ID       string `db:"id"`
 	Password string `db:"password"`
 	BaseEntity
+}
+
+// BuildFromModel ... build model from entity
+func (e *User) BuildFromModel(m *model.User) {
+	e.ID = m.ID
+	e.Password = m.Password
+}
+
+// OutputModel ... output model from entity
+func (e *User) OutputModel() *model.User {
+	return &model.User{
+		ID:       e.ID,
+		Password: e.Password,
+	}
 }

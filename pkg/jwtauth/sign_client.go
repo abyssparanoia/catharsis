ec8ca4461f3be26abc4d7ce454c25498a8cf4a0b
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2019-11-22 03:13:54 +0900

    feat: added sign client

---
+++ pkg/jwtauth/sign_client.go
@@ -0,0 +1,26 @@
+package jwtauth
+
+import (
+	"crypto/rsa"
+	"io/ioutil"
+
+	jwt "github.com/dgrijalva/jwt-go"
+)
+
+// SignClient ... jwt client for sign
+type SignClient struct {
+	signKey *rsa.PrivateKey
+}
+
+// NewClient ... get sign client
+func NewClient(signKeyPath string) *SignClient {
+	signBytes, err := ioutil.ReadFile(signKeyPath)
+	if err != nil {
+		panic(err)
+	}
+
+	signKey, err := jwt.ParseRSAPrivateKeyFromPEM(signBytes)
+	if err != nil {
+		panic(err)
+	}
+}

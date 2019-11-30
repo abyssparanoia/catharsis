d973aef17e60eaf444cee92ebb73b48b6508a51c
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2019-12-01 04:37:30 +0900

    feat: added request struct

---
+++ bff/infrastructure/internal/entity/authentication.go
@@ -0,0 +1,8 @@
+package entity
+
+import pb "github.com/abyssparanoia/catharsis/proto/authentication"
+
+// SignInRequest ... sign in request struct
+type SignInRequest struct {
+	pb.SignInMessageRequest
+}

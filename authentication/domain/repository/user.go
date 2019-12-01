a671ebc88e53598cf8298bb8a1ecd5f275881f4b
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2019-12-01 19:30:40 +0900

    feat: added create user repository inteface

--- authentication/domain/repository/user.go
+++ authentication/domain/repository/user.go
@@ -9,4 +9,12 @@ import (
 // User ... user repositoy interface
 type User interface {
 	Get(ctx context.Context, userID string) (*model.User, error)
+	Create(ctx context.Context, payload struct {
+		DisplayName         string
+		IconImagePath       string
+		BackgroundImagePath string
+		Profile             string
+		Email               string
+		CreatedAt           int64
+	}) (*model.User, error)
 }
